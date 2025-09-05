2a1cda19f3be8484af67a1bf0228b40445891122
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2025-09-05 23:46:20 +0100

    adding license stamp for later

---
+++ data/exampleLicenseWithModification.hpp
@@ -0,0 +1,14 @@
+/*
+ * ISDA CDS Standard Model
+ *
+ * Copyright (C) 2009 International Swaps and Derivatives Association, Inc.
+ * Developed and supported in collaboration with Markit
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the ISDA CDS Standard Model Public License.
+ *
+ * -------------------------------------------------------------------
+ * Modified by: Bernardo David Cohen Beraun, 2025
+ * Description: Refactored from C to C++. Just because is fun ;-).
+ * -------------------------------------------------------------------
+ */
