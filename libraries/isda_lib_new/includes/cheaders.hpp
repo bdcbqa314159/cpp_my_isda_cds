ee47cd29c34c0f603054935893bb1cfa82b6008d
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2025-09-15 00:50:51 +0100

    cleanup

--- libraries/isda_lib_new/includes/cheaders.hpp
+++ libraries/isda_lib_new/includes/cheaders.hpp
@@ -13,12 +13,6 @@
 
 #include <stdlib.h>
 
-// #include "cdate.hpp"
-// #include "cerror.hpp"
-// #include "cfinanci.hpp"
-// #include "cgeneral.hpp"
-// #include "cmemory.hpp"
-
 #ifdef __cplusplus
 extern "C" {}
 #endif
