543cc1adba65bb65807b171d6dcea40d00858d0a
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2025-09-18 23:34:14 +0100

    adding a banner in the Copyright message and updating with EXPORT_C

--- libraries/isda_lib_new/includes/bastypes.hpp
+++ libraries/isda_lib_new/includes/bastypes.hpp
@@ -6,105 +6,100 @@
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the ISDA CDS Standard Model Public License.
+ *
+ * -------------------------------------------------------------------
+ * Modified by: Bernardo David Cohen Beraun, 2025
+ * Description: Refactored from C to C++. Just because is fun ;-).
+ * -------------------------------------------------------------------
  */
 
 #ifndef BASTYPES_H
 #define BASTYPES_H
 
 #include "cdate.hpp"
+#include "code_cpp.hpp"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+EXPORT_C {
+  struct TRatePt {
+    TDate fDate;
+    double fRate;
+  };
 
-/*t
- */
-struct TRatePt {
-  TDate fDate;
-  double fRate;
-};
+  /*t
+   * Holds a zero-coupon rate curve or clean spread curve.
+   */
+  struct TCurve {
+    int fNumItems;      /* Number of TRatePts in fArray */
+    TRatePt *fArray;    /* Dates & rates */
+    TDate fBaseDate;    /* Discount date */
+    double fBasis;      /* Number compounding periods / year */
+    long fDayCountConv; /* How the year fraction is computed */
+  };
 
-/*t
- * Holds a zero-coupon rate curve or clean spread curve.
- */
-struct TCurve {
-  int fNumItems;      /* Number of TRatePts in fArray */
-  TRatePt *fArray;    /* Dates & rates */
-  TDate fBaseDate;    /* Discount date */
-  double fBasis;      /* Number compounding periods / year */
-  long fDayCountConv; /* How the year fraction is computed */
-};
+  /*t
+   * Defines a single fixed cashflow.
+   */
+  struct TCashFlow {
+    TDate fDate;
+    double fAmount;
+  };
 
-/*t
- * Defines a single fixed cashflow.
- */
-struct TCashFlow {
-  TDate fDate;
-  double fAmount;
-};
+  /*t
+   * CashFlowList is a list of CashFlows (date and amount)
+   */
+  struct TCashFlowList {
+    int fNumItems;
+    struct TCashFlow *fArray;
+  };
 
-/*t
- * CashFlowList is a list of CashFlows (date and amount)
- */
-struct TCashFlowList {
-  int fNumItems;
-  struct TCashFlow *fArray;
-};
+  /*t
+   * Defines dates required for one floating payment.
+   */
+  struct TCouponDates {
+    TDate resetDate;
+    TDate payDate;
+    TDate accrueStartDate;
+    TDate accrueEndDate;
+    TDate exDividendDate;
+  };
 
-/*t
- * Defines dates required for one floating payment.
- */
-struct TCouponDates {
-  TDate resetDate;
-  TDate payDate;
-  TDate accrueStartDate;
-  TDate accrueEndDate;
-  TDate exDividendDate;
-};
+  /*t
+   * Defines dates needed for a list of floating payments.
+   */
+  struct TCouponDateList {
+    int fNumItems;
+    TCouponDates *fArray;
+    long stubLocation;
+  };
 
-/*t
- * Defines dates needed for a list of floating payments.
- */
-struct TCouponDateList {
-  int fNumItems;
-  TCouponDates *fArray;
-  long stubLocation;
-};
-
-/*t
- * TFloatRate defines a floating rate. For example, a 5 year semi-annual
- * swap rate, LIBOR or a compounding rate.
- * Note that payInterval is ONLY used if rateType = JPMCDS_SIMPLE_BASIS.
- * This setting is used to compute LIBOR and par swap rates.
- * In order to compute compounding rates, set rateType = JPMCDS_ANNUAL_BASIS (1)
- * or 2 for semi-annual compounding, etc.
- * See the documentation for JpmcdsDayCountFraction for a list of possible
- * day count convention constants. Note that there is no difference
- * between a TFloatRate which has a payInterval *equal* to the
- * matInterval (for the same instrument) one which has a payInterval
- * which is *greater* than the matInterval, and one which has payInterval = 0.
- * This is because the instruments *always* have a payment at maturity,
- * and also payments at intervals of payInterval, for dates on or
- * before the maturity date.
- * Simple LIBOR, etc (a zero coupon rate) is expressed by making
- * the payInterval = matInterval.
- * Note that the spread is added to the rate *AFTER* multiplying by
- * the weight.
- * Note that the holiday file and bad day convention in the
- * TDateAdjIntvl are used to adjust dates used to compute the rate.
- */
-struct TFloatRate {
-  TDateInterval matInterval; /* Time to maturity of rate */
-  TDateInterval payInterval; /* Time between payments for rate */
-  long dayCountConv;         /* Day count convention of rate */
-  TDateAdjIntvl spotOffset;  /* From reset to rate effective date */
-  double spread;             /* Added to the rate  */
-  long rateType;             /* JPMCDS_SIMPLE_BASIS, JPMCDS_ANNUAL_BASIS*/
-  double weight;             /* Multiplied by rate */
-};
-
-#ifdef __cplusplus
+  /*t
+   * TFloatRate defines a floating rate. For example, a 5 year semi-annual
+   * swap rate, LIBOR or a compounding rate.
+   * Note that payInterval is ONLY used if rateType = JPMCDS_SIMPLE_BASIS.
+   * This setting is used to compute LIBOR and par swap rates.
+   * In order to compute compounding rates, set rateType = JPMCDS_ANNUAL_BASIS
+   * (1) or 2 for semi-annual compounding, etc. See the documentation for
+   * JpmcdsDayCountFraction for a list of possible day count convention
+   * constants. Note that there is no difference between a TFloatRate which has
+   * a payInterval *equal* to the matInterval (for the same instrument) one
+   * which has a payInterval which is *greater* than the matInterval, and one
+   * which has payInterval = 0. This is because the instruments *always* have a
+   * payment at maturity, and also payments at intervals of payInterval, for
+   * dates on or before the maturity date. Simple LIBOR, etc (a zero coupon
+   * rate) is expressed by making the payInterval = matInterval. Note that the
+   * spread is added to the rate *AFTER* multiplying by the weight. Note that
+   * the holiday file and bad day convention in the TDateAdjIntvl are used to
+   * adjust dates used to compute the rate.
+   */
+  struct TFloatRate {
+    TDateInterval matInterval; /* Time to maturity of rate */
+    TDateInterval payInterval; /* Time between payments for rate */
+    long dayCountConv;         /* Day count convention of rate */
+    TDateAdjIntvl spotOffset;  /* From reset to rate effective date */
+    double spread;             /* Added to the rate  */
+    long rateType;             /* JPMCDS_SIMPLE_BASIS, JPMCDS_ANNUAL_BASIS*/
+    double weight;             /* Multiplied by rate */
+  };
 }
-#endif
 
 #endif /* BASTYPES_H */
--- libraries/isda_lib_new/includes/buscache.hpp
+++ libraries/isda_lib_new/includes/buscache.hpp
@@ -6,17 +6,20 @@
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the ISDA CDS Standard Model Public License.
+ *
+ * -------------------------------------------------------------------
+ * Modified by: Bernardo David Cohen Beraun, 2025
+ * Description: Refactored from C to C++. Just because is fun ;-).
+ * -------------------------------------------------------------------
  */
 
 #ifndef BUSCACHE_H
 #define BUSCACHE_H
 
 #include "cdate.hpp"
+#include "code_cpp.hpp"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
+EXPORT_C {
 /*
 ** These constants define flags for determining whether a day is a weekend
 ** or not. This is to cope with countries for which weekends fall on Fridays
@@ -40,18 +43,18 @@ extern "C" {
 #define JPMCDS_WEEKEND_STANDARD \
   (JPMCDS_WEEKEND_SATURDAY | JPMCDS_WEEKEND_SUNDAY)
 
-/*t
-***************************************************************************
-** Contains holiday dates and a series of flags that indicate that
-** particular days of the week are always holidays.
-***************************************************************************
-*/
-struct THolidayList {
-  TDateList *dateList; /* date list of holidays */
-  long weekends;       /* weekends */
-};
+  /*t
+  ***************************************************************************
+  ** Contains holiday dates and a series of flags that indicate that
+  ** particular days of the week are always holidays.
+  ***************************************************************************
+  */
+  struct THolidayList {
+    TDateList *dateList; /* date list of holidays */
+    long weekends;       /* weekends */
+  };
 
-typedef struct _THolidayCache THolidayCache;
+  typedef struct _THolidayCache THolidayCache;
 
 /*m
 ***************************************************************************
@@ -62,121 +65,119 @@ typedef struct _THolidayCache THolidayCache;
 #define JPMCDS_IS_WEEKDAY(date, weekends) \
   (!(JPMCDS_IS_WEEKEND((date), (weekends))))
 
-/*
-***************************************************************************
-** This file is broken into two types of functions.
-**
-** 1. Holiday cache manipulation functions. These are functions that
-**    manipulate the holiday cache but perform no significant analytics.
-**
-** 2. Holiday list manipulation functions. These are functions that
-**    manipulate the holiday list structure but perform no significant
-**    analytics.
-***************************************************************************
-*/
+  /*
+  ***************************************************************************
+  ** This file is broken into two types of functions.
+  **
+  ** 1. Holiday cache manipulation functions. These are functions that
+  **    manipulate the holiday cache but perform no significant analytics.
+  **
+  ** 2. Holiday list manipulation functions. These are functions that
+  **    manipulate the holiday list structure but perform no significant
+  **    analytics.
+  ***************************************************************************
+  */
 
-/*
-***************************************************************************
-** 1. Holiday cache manipulation functions. These are functions that
-**    manipulate the holiday cache but perform no significant analytics.
-***************************************************************************
-*/
+  /*
+  ***************************************************************************
+  ** 1. Holiday cache manipulation functions. These are functions that
+  **    manipulate the holiday cache but perform no significant analytics.
+  ***************************************************************************
+  */
 
-/*f
-***************************************************************************
-** The function first checks if the input name is really an encoded
-** pointer. If it is, the function decodes the pointer and returns
-** it. Otherwise, it checks whether the name is "NONE" or "NO_WEEKENDS".
-** If it is either of those, the function returns a pointer to an
-** appropriate static data structure (for efficiency). If it is
-** none of the above, then the function searches for the name in the
-** cache. If it finds the name, it returns a pointer to the cached
-** holiday list. Otherwise, it interprets the name as a file name
-** and tries to load the holiday file from disk.
-**
-** You can use name = "NONE" or name = "NO_WEEKENDS" and the resulting
-** holiday list will be defined appropriately.
-**
-** This behaviour exactly duplicates the behaviour when using a holiday
-** name as an input to any analytics function.
-**
-** Returns NULL on failure, a valid THolidayList pointer on success.
-***************************************************************************
-*/
-THolidayList *JpmcdsHolidayListFromCache(
-    char *name); /* (I) Name associated with the holidays */
+  /*f
+  ***************************************************************************
+  ** The function first checks if the input name is really an encoded
+  ** pointer. If it is, the function decodes the pointer and returns
+  ** it. Otherwise, it checks whether the name is "NONE" or "NO_WEEKENDS".
+  ** If it is either of those, the function returns a pointer to an
+  ** appropriate static data structure (for efficiency). If it is
+  ** none of the above, then the function searches for the name in the
+  ** cache. If it finds the name, it returns a pointer to the cached
+  ** holiday list. Otherwise, it interprets the name as a file name
+  ** and tries to load the holiday file from disk.
+  **
+  ** You can use name = "NONE" or name = "NO_WEEKENDS" and the resulting
+  ** holiday list will be defined appropriately.
+  **
+  ** This behaviour exactly duplicates the behaviour when using a holiday
+  ** name as an input to any analytics function.
+  **
+  ** Returns NULL on failure, a valid THolidayList pointer on success.
+  ***************************************************************************
+  */
+  THolidayList *JpmcdsHolidayListFromCache(
+      char *name); /* (I) Name associated with the holidays */
 
-/*f
-***************************************************************************
-** Adds a holiday list to the holiday cache. If the entry already exists
-** in the cache, then the old version will be deleted.
-***************************************************************************
-*/
-int JpmcdsHolidayListAddToCache(
-    char *name,      /* (I) Name to associate holidays with */
-    THolidayList *hl /* (I) Adds shallow copy */
-);
+  /*f
+  ***************************************************************************
+  ** Adds a holiday list to the holiday cache. If the entry already exists
+  ** in the cache, then the old version will be deleted.
+  ***************************************************************************
+  */
+  int JpmcdsHolidayListAddToCache(
+      char *name,      /* (I) Name to associate holidays with */
+      THolidayList *hl /* (I) Adds shallow copy */
+  );
 
-/*f
-***************************************************************************
-** Empty holiday cache
-***************************************************************************
-*/
-void JpmcdsHolidayEmptyCache(void);
+  /*f
+  ***************************************************************************
+  ** Empty holiday cache
+  ***************************************************************************
+  */
+  void JpmcdsHolidayEmptyCache(void);
 
-/*
-***************************************************************************
-** 2. Holiday list manipulation functions. These are functions that
-**    manipulate the holiday list structure but perform no significant
-**    analytics.
-***************************************************************************
-*/
+  /*
+  ***************************************************************************
+  ** 2. Holiday list manipulation functions. These are functions that
+  **    manipulate the holiday list structure but perform no significant
+  **    analytics.
+  ***************************************************************************
+  */
 
-/*f
-***************************************************************************
-** Creates a new holiday structure.
-**
-** The date list can be NULL, in which case the resulting date list in the
-** holiday structure will be a date list with no dates, e.g.
-**      hl->dateList->fNumItems = 0;
-***************************************************************************
-*/
-THolidayList *JpmcdsHolidayListNewGeneral(
-    TDateList *dateList, /* (I) Date list to use */
-    long weekends        /* (I) Weekends flag - use JPMCDS_WEEKEND_... */
-);
+  /*f
+  ***************************************************************************
+  ** Creates a new holiday structure.
+  **
+  ** The date list can be NULL, in which case the resulting date list in the
+  ** holiday structure will be a date list with no dates, e.g.
+  **      hl->dateList->fNumItems = 0;
+  ***************************************************************************
+  */
+  THolidayList *JpmcdsHolidayListNewGeneral(
+      TDateList * dateList, /* (I) Date list to use */
+      long weekends         /* (I) Weekends flag - use JPMCDS_WEEKEND_... */
+  );
 
-/*f
-***************************************************************************
-** Deletes a holiday list structure.
-***************************************************************************
-*/
-void JpmcdsHolidayListDelete(THolidayList *hl); /* (I) Holiday list to delete */
+  /*f
+  ***************************************************************************
+  ** Deletes a holiday list structure.
+  ***************************************************************************
+  */
+  void JpmcdsHolidayListDelete(THolidayList *
+                               hl); /* (I) Holiday list to delete */
 
-/*f
-***************************************************************************
-** Reads a holiday file into memory as a holiday list.
-**
-** Structure of holiday file, ascii text file of lines like:
-**     #            - commment (blank lines are also skipped)
-**     19631026     - means 10/26/1963 is a holiday
-**     # SATURDAY_NOT_ALWAYS_HOLIDAY  - sets "saturday isn't always a holiday"
-**     # SUNDAY_NOT_ALWAYS_HOLIDAY    - sets "sunday isn't always a holiday"
-**     # MONDAY_ALWAYS_HOLIDAY        - sets "monday as always a holiday"
-**     # TUESDAY_ALWAYS_HOLIDAY       - sets "tuesday as always a holiday"
-**     # WEDNESDAY_ALWAYS_HOLIDAY     - sets "wednesday as always a holiday"
-**     # THURDSAY_ALWAYS_HOLIDAY      - sets "thursday as always a holiday"
-**     # FRIDAY_ALWAYS_HOLIDAY        - sets "friday as always a holiday"
-**
-** Dates must be in increasing order.
-***************************************************************************
-*/
-THolidayList *JpmcdsHolidayListRead(
-    char *fileName /* (I) Name of file to read (may differ) */
-);
-
-#ifdef __cplusplus
+  /*f
+  ***************************************************************************
+  ** Reads a holiday file into memory as a holiday list.
+  **
+  ** Structure of holiday file, ascii text file of lines like:
+  **     #            - commment (blank lines are also skipped)
+  **     19631026     - means 10/26/1963 is a holiday
+  **     # SATURDAY_NOT_ALWAYS_HOLIDAY  - sets "saturday isn't always a holiday"
+  **     # SUNDAY_NOT_ALWAYS_HOLIDAY    - sets "sunday isn't always a holiday"
+  **     # MONDAY_ALWAYS_HOLIDAY        - sets "monday as always a holiday"
+  **     # TUESDAY_ALWAYS_HOLIDAY       - sets "tuesday as always a holiday"
+  **     # WEDNESDAY_ALWAYS_HOLIDAY     - sets "wednesday as always a holiday"
+  **     # THURDSAY_ALWAYS_HOLIDAY      - sets "thursday as always a holiday"
+  **     # FRIDAY_ALWAYS_HOLIDAY        - sets "friday as always a holiday"
+  **
+  ** Dates must be in increasing order.
+  ***************************************************************************
+  */
+  THolidayList *JpmcdsHolidayListRead(
+      char *fileName /* (I) Name of file to read (may differ) */
+  );
 }
-#endif
 
 #endif /* BUSCACHE_H */
